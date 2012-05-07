35804b7ab0b61f1e1339562f6fe5afbd999995c0
Xavier Dekeyster <xavier@dx-solutions.be>
2012-05-07 17:46:51 +0200

    login system

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -15,20 +15,45 @@ class UsersController extends Zend_Controller_Action
 
     public function loginAction()
     {
-        $this->view->form = $this->view->loginFormulier();
+        $this->view->form = new Application_Form_Login();
+
+
          // controle en mail versturen
         if ($this->getRequest()->isPost()) {
-            // haal alle post variabelen op 
+            // haal alle post variabelen op
             $postParams = $this->getRequest()->getPost();
             if ($this->view->form->isValid($postParams)){
+
                 $params = $this->view->form->getValues();
-                echo '<pre>';
-                print_r($this->view->form->getErrors());
-                echo '</pre>';
-                die();
+
+                $auth = Zend_Auth::getInstance();
+
+                $authAdapter = new Zend_Auth_Adapter_DbTable(Zend_Registry::get('db'));
+                $authAdapter->setTableName('users') // table name
+                        ->setIdentityColumn('username') // veld gebruikersnaam
+                        ->setCredentialColumn('password') // veld wachtwoord
+                        ->setIdentity($params['login']) // form info
+                        ->setCredential(md5($params['password']));
+                $authResult = $auth->authenticate($authAdapter);
+
+
+                if($authResult->isValid()){
+                    $identity = Zend_Auth::getInstance()->getIdentity();
+
+                    echo 'Ingelogd als ' . $identity;
+                    // get userdata based on hte identity
+                }else{
+                    // TODO define errors
+                    foreach ($authResult->getMessages() as $message) {
+                        echo $message . '<br />';
+                    }
+                    $this->_redirect('/');
+                }
+
             }else{
                 $loginErrors = new Zend_Session_Namespace('loginErrors');
                 $loginErrors->errors = $this->view->form->getErrors();
+
             }
         }
     }
--- application/forms/Login.php
+++ application/forms/Login.php
@@ -6,12 +6,12 @@ class Application_Form_Login extends Zend_Form
     public function init()
     {
         $l = Zend_Registry::get('Zend_Locale');
-        $url = $l . '/login';
+        $url = '/'.$l . '/login';
         // formulier aanmaken
         $this->setMethod('post');
         $this->setAttrib('enctype', 'multipart/form-data');
         $this->setAction($url);
-      
+
         // LOGIN
         $this->addElement(new Zend_Form_Element_Text('login',array(
             'label' => 'label.login',
@@ -24,14 +24,14 @@ class Application_Form_Login extends Zend_Form
             'filters' => array('stringTrim'),
             'required' => true
         )));
-        
+
         $btn = new Zend_Form_Element_Button('submit',array(
             'type' => 'submit',
             'value' => 'label.login',
             'required' => false,
             'ignore'  => true,
         ));
-        
+
         $this->addElement($btn);
     }
 
